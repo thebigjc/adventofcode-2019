7963486b9f0f2377bf05d05c16422832c5ce46df
thebigjc <jordanc@ecobee.com>
2019-12-10 00:22:34 -0500

    day9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -10,7 +10,6 @@ fn parse(s: &str) -> i128 {
 
 
 struct Program {
-    orig: Vec<i128>,
     tokens: HashMap<i128, i128>,
     inputs: VecDeque<i128>,
     output: i128,
@@ -22,7 +21,6 @@ struct Program {
 impl Program {
     fn new(t: &Vec<i128>) -> Program {
         Program {
-            orig: t.to_vec(),
             tokens: HashMap::from_iter(t.iter().enumerate().map(|(a,b)|(a as i128, *b))),
             inputs: VecDeque::new(),
             output: i128::MIN,
@@ -58,17 +56,8 @@ impl Program {
         }
     }
 
-    /*fn reset(&mut self) {
-        self.tokens = self.orig.to_vec();
-        self.i = 0;
-        self.rb = 0;
-        self.inputs = VecDeque::new();
-        self.output = i128::MIN;
-        self.halted = false;
-    }*/
-
     fn store(&mut self, i: i128, x: i128) {
-        println!("Store: {} = {}", i, x);
+        //println!("Store: {} = {}", i, x);
         self.tokens.insert(i,x);
     }
 
@@ -76,11 +65,12 @@ impl Program {
         &mut self,
         a_mode: fn(&Program, i128) -> i128,
         b_mode: fn(&Program, i128) -> i128,
+        rb: i128,
         o: fn(a: i128, b: i128) -> i128,
     ) {
         let a = a_mode(self, 1);
         let b = b_mode(self, 2);
-        let c = self.immediate(3) as i128;
+        let c = self.immediate(3) + rb;
         self.store(c, o(a,b));
         self.i += 4;
     }
@@ -103,11 +93,12 @@ impl Program {
         &mut self,
         a_mode: fn(&Program, i128) -> i128,
         b_mode: fn(&Program, i128) -> i128,
+        rb: i128,
         o: fn(a: i128, b: i128) -> bool,
     ) {
         let a = a_mode(self, 1);
         let b = b_mode(self, 2);
-        let c = self.immediate(3) as i128;
+        let c = self.immediate(3) + rb;
         if o(a, b) {
             self.store(c, 1);
         } else {
@@ -122,17 +113,27 @@ impl Program {
             let opcode = t % 100;
             let a_mode = self.mode((t / 100) % 10);
             let b_mode = self.mode((t / 1000) % 10);
-            println!("rb = {}, a_mode = {}, b_mode = {}, t = {}, opcode = {}", self.rb, (t / 100) % 10, (t / 1000) % 10, t, opcode);
+            let am = (t / 100) % 10;
+            let cm = (t / 10000) % 10;
+            let mut rb = 0;
+            if cm == 2 {
+                rb = self.rb;
+            }
+
+            //println!("rb = {}, a_mode = {}, b_mode = {}, t = {}, opcode = {}", self.rb, (t / 100) % 10, (t / 1000) % 10, t, opcode);
 
             match opcode {
                 1 => {
-                    self.op(a_mode, b_mode, |a, b| a + b);
+                    self.op(a_mode, b_mode, rb, |a, b| a + b);
                 }
                 2 => {
-                    self.op(a_mode, b_mode, |a, b| a * b);
+                    self.op(a_mode, b_mode, rb, |a, b| a * b);
                 }
                 3 => {
-                    let a = a_mode(self, 1) as i128;
+                    let mut a = self.immediate(1);
+                    if am == 2 {
+                        a += self.rb;
+                    }
                     let i = self.inputs.pop_front().unwrap();
                     self.store(a, i);
                     self.i += 2;
@@ -149,10 +150,10 @@ impl Program {
                     self.branch(a_mode, b_mode, |a| a == 0);
                 }
                 7 => {
-                    self.test(a_mode, b_mode, |a, b| a < b);
+                    self.test(a_mode, b_mode, rb, |a, b| a < b);
                 }
                 8 => {
-                    self.test(a_mode, b_mode, |a, b| a == b);
+                    self.test(a_mode, b_mode, rb, |a, b| a == b);
                 }
                 9 => {
                     let x = a_mode(self, 1);
@@ -175,7 +176,7 @@ impl Program {
 fn one(t: &Vec<i128>) -> Vec<i128> {
     let mut p = Program::new(t);
     let mut output = Vec::new();
-    p.inputs.push_back(1);
+    p.inputs.push_back(2);
     loop {
         p.intcode();
         if p.halted {
@@ -239,5 +240,17 @@ mod tests {
         let ret = p.output;
 
         assert_eq!(1125899906842624, ret);
+    }
+    #[test]
+    fn test_one_4() {
+        let input = vec![
+            109, 1, 203, 2, 204, 2, 99
+        ];
+        let mut p = Program::new(&input);
+        p.inputs.push_back(1024);
+        p.intcode();
+        let ret = p.output;
+
+        assert_eq!(1024, ret);
     }
 }
